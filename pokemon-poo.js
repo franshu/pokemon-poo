4221ea988979ca6b5a65dfb18439f46db8dc43b9
franshu <fran.ojeda208@gmail.com>
2017-05-04 11:06:42 -0300

    finished pokemon

--- pokemon-poo.js
+++ pokemon-poo.js
@@ -1,27 +1,26 @@
-function Pokemon(nombre,color,vida,poderDeAtaque){
+function Pokemon(nombre,color,nivelDeAmistad,vida,poderDeAtaque){
 	this.nombre = nombre;
 	this.color = color;
 
-	this.nivelDeAmistad = 0
+	this.nivelDeAmistad = nivelDeAmistad;
 
 	this.vida = vida;
 	this.poderDeAtaque = poderDeAtaque;
 
 	this.mostrarPokemon = function(){
-		return('Hola,soy:' + this.nombre + ' y soy de color: '+ this.color)
+		return("Hola, soy:" + this.nombre + " y soy de color: "+ this.color)
 	}
 	this.aumentarAmistad = function(valor){
 		this.nivelDeAmistad = this.nivelDeAmistad + valor
 	}
 	this.atacar = function(pokemon){
-		pokemon.vida = pokemon.vida + this.poderDeAtaque
+		pokemon.vida = pokemon.vida - this.poderDeAtaque
 	}
 }
 
-var Pikachu = new Pokemon('Pikachu','amarillo',150, 100);
-var Charmander = new Pokemon ('Charmander','rojo',200, 20);
+var pikachu = new Pokemon("Pikachu","amarillo",5,150,100);
+var charmander = new Pokemon("Charmander","rojo",3,200,20);
 
-Pikachu.atacar(Charmander);
+pikachu.atacar(charmander);
 
-console.log(Charmander.vida)
-console.log(Pikachu.aumentarAmistad())
+console.log(charmander.vida);
